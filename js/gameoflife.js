e89356f11baaf2f885962c0efa6b62e67e088232
victor <vajor@mgic.umaryland.edu>
2021-10-01 12:45:27 +0100

    Completed module

--- js/gameoflife.js
+++ js/gameoflife.js
@@ -1,27 +1,104 @@
-function seed() {}
+//complete the function named seed that returns its arguments in an array.
+function seed(...arg) { return arg}
 
-function same([x, y], [j, k]) {}
+//Complete the function named same that accepts two cells 
+//and returns a Boolean indicating if the two cells are the same.
+function same([x, y], [j, k]) { return x === j && y === k}
 
+// Complete the function named contains that tests if the supplied cell is alive in the passed game state.
 // The game state to search for `cell` is passed as the `this` value of the function.
-function contains(cell) {}
+function contains(cell) { return this.some(x => same(x,cell))}
 
-const printCell = (cell, state) => {};
+// Complete the function printCell that returns a string representation of a cell's state (is it alive or not?). 
+const printCell = (cell, state) => { return contains.call(state, cell) ? "\u25A3" : "\u25A2"};
 
-const corners = (state = []) => {};
+//Complete the function corners that calculates the top-right and bottom-left 
+//coordinates of the smallest rectangle that contains all living cells. 
+const corners = (state = []) => {
+  if (state.length === 0) {
+    return {
+      topRight: [0,0],
+      topLeft: [0,0]
+    }
+  }
 
-const printCells = (state) => {};
+  const xs = state.map(([x, _]) => x);
+  const ys = state.map(([y, _]) => y);
 
-const getNeighborsOf = ([x, y]) => {};
+  return {
+    topRight: [Math.max(...xs), Math.max(...ys)],
+    topLeft: [Math.min(...xs), Math.min(...ys)]
+  }
+};
+//Complete the function printCells that uses the printCell and corners functions 
+//completed previously to build a string representation of the game state. 
+const printCells = (state) => {
+  const { bottomLeft, topRight} = corners(state)
+  let accumulator = "";
+  for (let y = topRight[1]; y >= bottomLeft[1]; y--) {
+    let row = [];
+    for(let x = bottomLeft[0]; x <= topRight[0]; x++){
+      row.push(printCell([x,y], state))
+    }
+    accumulator += row.join(" ") + "\n";
+  }
 
-const getLivingNeighbors = (cell, state) => {};
+  return accumulator;
+};
 
-const willBeAlive = (cell, state) => {};
+//Complete the single-line arrow function getNeighborsOf that 
+//returns an array containing all of the neighbors of a given cell.
 
-const calculateNext = (state) => {};
+const getNeighborsOf = ([x, y]) => {
+  [x-1, y+1], [x, y+1], [x+1, y+1],
+  [x-1, y],             [x+1, y],
+  [x-1, y-1], [x, y-1], [x+1, y-1]
+};
 
-const iterate = (state, iterations) => {};
+//Complete the function getLivingNeighbors that 
+//returns the living neighbors of a given cell within a given game state. 
+const getLivingNeighbors = (cell, state) => {
+  return getNeighborsOf(cell).filter((n) => contains.bind(state)(n));
+};
 
-const main = (pattern, iterations) => {};
+//Complete the function willBeAlive that calculates if a given cell will be alive in the next game state.
+const willBeAlive = (cell, state) => {
+  const livingNeighbors = getLivingNeighbors(cell, state);
+
+  return(
+    livingNeighbors.length === 3 || (contains.call(state,cell) &&
+    livingNeighbors.length === 2)
+  )
+};
+
+//Complete the function calculateNext that calculates the next state of the game from the current state of the game.
+const calculateNext = (state) => {
+  const {bottomLeft, topRight}  = corners(state);
+  let result = [];
+  for (let y = topRight[1] + 1; y >= bottomLeft[1] - 1; y-- ) {
+    for(let x = bottomLeft[0] - 1; x <= topRight[0] + 1; x++) {
+      result = result.concat(willBeAlive([x,y], state) ? [[x, y]] : []);
+    }
+  }
+  return result;
+};
+
+//Complete the function iterate that calculates new game states, based on a starting game state.
+const iterate = (state, iterations) => {
+  const states = [state];
+  for(let i = 0; i < iterations; i++) {
+    states.push(calculateNext(states[states.length - 1]));
+  }
+
+  return states;
+};
+
+//Complete the function main that calculates a given number of future states from a given starting 
+//states and prints them all to the console (including the initial state).
+const main = (pattern, iterations) => {
+  const results = iterate(startPatterns[pattern], iterations);
+  results.forEach(a => console.log(printCells(a)));
+};
 
 const startPatterns = {
     rpentomino: [
